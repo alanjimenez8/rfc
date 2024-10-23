557b319d140fcea36af2f3533c904fac6c247c27
alanjimenez8 <ruja080816hgtzmla0@cbtis217.edu.mx>
2024-10-23 16:22:59 +0200

    finish

--- w/w/MainForm.Designer.cs
+++ w/w/MainForm.Designer.cs
@@ -36,12 +36,82 @@ namespace w
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.Location = new System.Drawing.Point(255, 299);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(149, 30);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "finalizar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.Location = new System.Drawing.Point(268, 183);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "ingresar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Location = new System.Drawing.Point(0, 3);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 2;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+									"nombre",
+									"apellido_paterno",
+									"apellido_materno",
+									"telefono",
+									"direccion",
+									"semestre"});
+			this.comboBox1.Location = new System.Drawing.Point(12, 186);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(106, 21);
+			this.comboBox1.TabIndex = 4;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(148, 186);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 5;
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "w";
+			this.ClientSize = new System.Drawing.Size(445, 341);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.monthCalendar1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
+			this.Text = "w";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
 	}
 }
--- w/w/MainForm.cs
+++ w/w/MainForm.cs
@@ -29,5 +29,46 @@ namespace w
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		string [] s = new string[5];
+		string d;
+		void Button2Click(object sender, EventArgs e)
+		{
+			int a=comboBox1.SelectedIndex;
+			s[a]=string.Empty;
+			s[a]=textBox1.Text;
+			textBox1.Text=string.Empty;
+		}
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			string a,b,c;
+			a=s[0];
+			b=s[1];
+			c=s[2];
+			d+=b[0];
+			d+=b[1];
+			d+=c[0];
+			d+=a[0];
+			DateTime fechaSeleccionada = monthCalendar1.SelectionStart;
+			int  numeroDelaño = fechaSeleccionada.Year;
+			string f=numeroDelaño.ToString();
+			d+=f[2];
+			d+=f[3];
+			int numeroDelmes = fechaSeleccionada.Month;
+			if(numeroDelmes<10){
+				d+='0';
+			}
+			f=numeroDelmes.ToString();
+			d+=f;
+			int numeroDeldia = fechaSeleccionada.Day;
+			if(numeroDeldia<10){
+				d+='0';
+			}
+			f=numeroDeldia.ToString();
+			d+=f;
+			MessageBox.Show(d);
+			d=string.Empty;
+		}
 	}
 }
